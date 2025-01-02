98f2eda170ecc7bc839c3c05fdaa4c707b9af6d9
LuizFelixDev <luizhenriquefelix138@gmail.com>
2025-01-01 23:59:50 -0300

    Iniciando a programação

---
+++ AULA16ex/EX18/script.js
@@ -0,0 +1,13 @@
+function analisador(){
+    let tab = window.document.getElementById('tab')
+    let num = window.document.getElementById('inum')
+    let res = window.document.getElementById('res')
+    let n = Number(num.value)
+
+    if(n == 0 ){
+        window.alert(`O número inserido é invalido`)
+    }
+    else{
+        alert('Tudo ok!')
+    }
+}
