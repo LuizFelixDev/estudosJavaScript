6592f39ff76fce03d6de4ec5cfbf615755fa83a8
LuizFelixDev <luizhenriquefelix138@gmail.com>
2025-01-31 16:34:00 -0300

    Implemetei um verificador básico

--- EXPRATICOS/app.js
+++ EXPRATICOS/app.js
@@ -3,9 +3,13 @@ let lista = []
 
 function adicionar(){
     let valor = window.document.getElementById('listaNum').value
+    if(valor == ''){
+        window.document.getElementById('lista').innerHTML = 'O valor digitado é inválido!'
+    }else{
     lista.push(valor);
     let listatxt = window.document.getElementById('lista')
     listatxt.innerHTML = 'lista = ' + lista
+    }
 }
 
 function refazer(){
