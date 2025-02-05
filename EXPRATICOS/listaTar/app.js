a14e533abd44a73c3da86295ab146e9af3ebf991
LuizFelixDev <luizhenriquefelix138@gmail.com>
2025-02-05 10:10:09 -0300

    Melhorando o título

--- EXPRATICOS/listaTar/app.js
+++ EXPRATICOS/listaTar/app.js
@@ -1,3 +1,18 @@
+
+function titulo(){
+const  dataAtual = new Date()
+const diaMes = dataAtual.getDate()
+const mes = dataAtual.getMonth()
+switch(mes){
+    
+}
+
+titulo = window.document.getElementById('titulo')
+titulo.innerHTML = `Lista de tarefas de ${diaMes} de ${mes}`
+}
+
+document.addEventListener("DOMContentLoaded", titulo);
+
 function adicionar(){
     let lista = window.document.getElementById('minhaLista')
     let novo = window.document.createElement('li')
@@ -14,3 +29,4 @@ function limpar(){
     lista = window.document.getElementById('minhaLista')
     lista.innerHTML = ''
 }
+
